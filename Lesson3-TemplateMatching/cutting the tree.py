2616d3d6b57671fca5ae6f0e79c0f21c805aee80
Samer98 <samerhisham.work@gmail.com>
2021-09-26 13:04:25 +0200

    House cleaning

--- Lesson3-TemplateMatching/cutting the tree.py
+++ Lesson3-TemplateMatching/cutting the tree.py
@@ -4,92 +4,30 @@ import cv2 as cv
 import numpy as np
 from time import time, sleep
 import pyautogui
-from enum import Enum
-
-class Direction(Enum):
-    left = 1
-    right = 2
 
 
 class wood():
-    def __init__(self):
-        self.woodleft = cv.imread('woodleft.jpg')
-        self.woodright = cv.imread('woodright.jpg')
-
+    def __init__(self,image):
+        self.wood = cv.imread(image)
         self.top = 0
         self.left = 0
-        self.loc = 0
-        self.width = self.woodleft.shape[1]
-        self.height = self.woodleft.shape[0]
 
-    def match_wood_left(self, screen):
+        self.width = self.wood.shape[1]
+        self.height = self.wood.shape[0]
+
+    def match_wood(self, screen):
         scr_remove = screen[:,:,:3]
-        result= cv.matchTemplate(scr_remove,self.woodleft,cv.TM_CCOEFF_NORMED)
-        _, _, _, loc = cv.minMaxLoc(result)
+        result= cv.matchTemplate(scr_remove, self.wood, cv.TM_CCOEFF_NORMED)
+        _, _, _, self.loc = cv.minMaxLoc(result)
 
         threshold = 0.48
-        top , left = np.where(result >= threshold)
-        return top , left , loc
-    def match_wood_right(self, screen):
-        scr_remove = screen[:,:,:3]
-        result= cv.matchTemplate(scr_remove,self.woodright,cv.TM_CCOEFF_NORMED)
-        _, _, _, loc = cv.minMaxLoc(result)
-
-        threshold = 0.48
-        top ,left = np.where(result >= threshold)
-        return top , left , loc
+        self.top , self.left = np.where(result >= threshold)
     def location(self):
         return self.loc
-
-    def draw(self,screen,left,top):
-        for x,y in zip(left,top):
+    def draw(self,screen):
+        for x,y in zip(self.left,self.top):
             cv.rectangle(screen,(x,y),(x+self.width,y+self.height),(0,0,255),2)
 
-
-# class woodright():
-#     def __init__(self):
-#         self.wood = cv.imread('woodright.jpg')
-#         self.top = 0
-#         self.left = 0
-#         self.loc = 0
-#         self.width = self.wood.shape[1]
-#         self.height = self.wood.shape[0]
-#
-#     def match_wood(self, screen):
-#         scr_remove = screen[:,:,:3]
-#         result= cv.matchTemplate(scr_remove,self.wood,cv.TM_CCOEFF_NORMED)
-#         _, _, _, self.loc = cv.minMaxLoc(result)
-#
-#         threshold = 0.5
-#         self.top , self.left = np.where(result >= threshold)
-#
-#     def location(self):
-#         return self.loc
-#
-#     def draw(self,screen):
-#         for x,y in zip(self.left,self.top):
-#             cv.rectangle(screen,(x,y),(x+self.width,y+self.height),(0,0,255),2)
-
-class Player():
-    def __init__(self):
-        self.direction = Direction.left
-
-    def go_left(self):
-        pyautogui.click(1536,637)
-        self.direction = Direction.left
-
-    def go_right(self):
-        pyautogui.click(1794,637)
-        self.direction = Direction.right
-
-    def chop_ya_kick(self):
-        if(self.direction == Direction.left):
-            self.go_left()
-        else:
-            self.go_right()
-
-# woodleft = cv.imread('woodleft.jpg')
-# woodright= cv.imread('woodright.jpg')
 
 x=1536
 y=450
@@ -101,26 +39,28 @@ dimensions = {
         'width': 500,
         'height': 100
     }
+woodleft= "woodleft.jpg"
+woodright= "woodright.jpg"
 
-player = Player()
-wood = wood()
-
+woodleft_obj= wood(woodleft)
+woodright_obj = wood(woodright)
 while True:
     scr = np.array(sct.grab(dimensions))
-    top_rightwood , left_rightwood , loc_rightwood =wood.match_wood_right(scr)
-    wood.draw(scr,left_rightwood,top_rightwood)
-    top_leftwood , left_leftwood ,loc_leftwood =wood.match_wood_left(scr)
-    wood.draw(scr,left_leftwood,top_leftwood)
-    # print(loc_leftwood[1])
-    # print(loc_rightwood[1])
-    if  loc_leftwood[1] == 11:
+    woodleft_obj.match_wood(scr)
+    woodleft_obj.draw(scr)
+
+
+    woodright_obj.match_wood(scr)
+    woodright_obj.draw(scr)
+
+    if woodleft_obj.loc[0] == 101:
         x=1794
 
-    elif loc_rightwood[1] == 12:
+    elif woodright_obj.loc[0] == 277:
         x = 1536
 
     pyautogui.click(x=x, y=y)
-    sleep(.10)
+    sleep(.01)
     cv.imshow('screen',scr)
     cv.waitKey(1)
 
